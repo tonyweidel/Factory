68dd49674d381017c859ac9f833d8687f570cf41
Tony Weidel <8aweidel@stpatrick.org>
2017-11-07 14:43:45 -0600

    mitch is a cis male

--- worker.js
+++ worker.js
@@ -1,8 +1,12 @@
-const thing = require('./thing.js');
+const Random = require('./Random.js');
 
 const Worker = function(){
+  let id = "w" + random().digit(8);
+     let wage = random().continuousRange(8,50);
+     let hours = random().continuousRange(4,7)*5;
 
-
-};
-
+     function pay(){
+       console.log(wage*8, wage*40, wage*2080)
+     }
+}
 module.exports = moduleName;
